d1e3565cd0796d9f240ab391282a8e2ccfa215e1
Konstantinos Tsolakidis <konstantinos@humanelectric.co>
2018-03-10 15:52:57 +0200

    edit start

--- src/managers/apiManager.js
+++ src/managers/apiManager.js
@@ -1,8 +1,8 @@
 // import fetch from 'cross-fetch'
 
-
+const host = process.env.NODE_ENV === 'production' ? 'https://finance-gen-api.herokuapp.com' : 'http://localhost:3100';
 export const getAllData = () => {
     console.log(' calling get data')
-    return fetch('http://localhost:3100/userdata')
+    return fetch(host + '/userdata')
         .then(res => res.json());
 };
